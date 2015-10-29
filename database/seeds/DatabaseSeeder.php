702c35e6141357faeda2aa0220f6cccae8522846
Wouerner <wouerner@gmail.com>
2015-10-29 10:41:38 -0200

    [CLEAN] Retirando vendor e adicionando composer.json e lock

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,10 +14,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Model::unguard();
+ Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        DB::table('users')->delete();
 
-        Model::reguard();
-    }
+        $users = array(
+                ['name' => 'Ryan Chenkie', 'email' => 'ryanchenkie@gmail.com', 'password' => Hash::make('secret')],
+                ['name' => 'Chris Sevilleja', 'email' => 'chris@scotch.io', 'password' => Hash::make('secret')],
+                ['name' => 'Holly Lloyd', 'email' => 'holly@scotch.io', 'password' => Hash::make('secret')],
+                ['name' => 'Adnan Kukic', 'email' => 'adnan@scotch.io', 'password' => Hash::make('secret')],
+        );
+            
+        // Loop through each user above and create the record for them in the database
+        foreach ($users as $user)
+        {
+            User::create($user);
+        }
+
+        Model::reguard();    }
 }
